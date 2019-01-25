a7e7d55884092cbe3ea210d5ce995e0917fcb928
Pirun Seng <sengpirun.rain@gmail.com>
2019-01-25 15:46:20 +0700

    Add confirm message to delete product

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
   $('.deleteProduct').on('click', function(e){
-    // debugger
     pid = $(this).data('id')
     url = '/products/delete/' + pid;
-    // console.log(url);
-    $.ajax({
-      url: url,
-      type: 'DELETE',
-      success: function(result){
-        console.log('deleted success!')
-      }
-    });
-    window.location = '/';
+
+    var confirmed = confirm("Are you sure?");
+    if(confirmed){
+      $.ajax({
+        url: url,
+        type: 'DELETE',
+        success: function(result){
+          console.log('deleted success!')
+        }
+      });
+      window.location = '/';
+    }
   });
 });
